90ee45d95c26e5d7599d02108528c69033c30934
Viktor Herman <viktor.herman@vh-ws.local>
2024-06-09 20:53:23 +0200

    Rearrange items on FirstScreen, update_time() - working

--- Screens.py
+++ Screens.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from datetime import datetime
 
 
 # -----------------------------
@@ -10,80 +11,115 @@ FONT_SUBTITLE = ("Arial", 18)
 FONT_POSTAVKE = ("Arial", 16)
 FONT_STANDARD = ("Arial", 14)
 
-RASVJETA_PLACEHOLDER = "UPRAVLJANJE RASVJETOM"
-TEMPERATURA_PLACEHOLDER = "UPRAVLJANJE TEMPERATUROM"
-KAMERA_PLACEHOLDER = "UPRAVLJANJE KAMEROM"
-DODATNE_METRIKE_PLACEHOLDER = "UPRAVLJAJ DODATNIM MJERENJIMA"
-LOKACIJA_PLACEHOLDER = "UPRAVLJAJ LOKACIJOM"
+RASVJETA_PLACEHOLDER = "RASVJETA"
+TEMPERATURA_PLACEHOLDER = "TEMP"
+KAMERA_PLACEHOLDER = "KAMERA"
+VLAZNOST_PLACEHOLDER = "VLAZNOST"
+TLAK_PLACEHOLDER = "TLAK"
+
+LOKACIJA_PLACEHOLDER = "LOKACIJA"
 
 
 class FirstScreen:
-    def __init__(self, root: tk.Tk, screen_size: str = "600x400") -> None:
+    def __init__(self, root: tk.Tk, screen_size: str = "800x300") -> None:
         self.root = root
         self.root.title = "SmartHome"
         self.root.geometry(screen_size)
 
         # -----------------------------
-        # SCREEN POSTAVKE
+        # PRIKAZ DATUMA
+        # -----------------------------
+        self.label_datum = tk.Label(root, text="Datum, loading...")
+        self.label_datum.place(relx=0.025, rely=0.05, anchor="w")
+        # TODO: Napraviti funkciju koja update-a self.label_datum
+
+        # -----------------------------
+        # PRIKAZ VANJESKE TEMPERATURE
+        # -----------------------------
+        self.label_vanjska_temp = tk.Label(root, text="Vanjska temperatura, loading...")
+        self.label_vanjska_temp.place(relx=0.95, rely=0.05, anchor="e")
+        # TODO: Napraviti funkciju koja update-a self.label_vanjska_temp
+
+        # -----------------------------
+        # GLAVNI IZBORNIK
         # -----------------------------
 
         self.label_postavke = ttk.Label(
-            self.root, text="POSTAVKE SUSTAVA", font=FONT_TITLE
+            self.root, text="GLAVNI IZBORNIK", font=FONT_TITLE
         )
-        self.label_postavke.place(relx=0.5, rely=0.05, anchor="center")
+        self.label_postavke.place(relx=0.5, rely=0.15, anchor="center")
 
         # -----------------------------
         # MOGUCE POSTAVKE
         # -----------------------------
-        BUTTON_WIDTH = 500
-        FIRST_REL_Y_PLACEMENT = 0.15
+        BUTTON_WIDTH = 75
+        BUTTON_HEIGHT = 75
+
         # Upravljanje rasvjetom - button
-        self.button_upravljanje_rasvjetom = ttk.Button(
+        self.button_upravljanje_rasvjetom = tk.Button(
             self.root,
             text=RASVJETA_PLACEHOLDER,
         )
         self.button_upravljanje_rasvjetom.place(
-            relx=0.5, rely=FIRST_REL_Y_PLACEMENT, width=BUTTON_WIDTH, anchor="center"
+            relx=0.25,
+            rely=0.25,
+            height=BUTTON_HEIGHT,
+            width=BUTTON_WIDTH,
         )
 
         # Upravljanje temperaturom - button
-        self.button_upravljanje_temperaturom = ttk.Button(
-            self.root, text=TEMPERATURA_PLACEHOLDER
+        self.button_upravljanje_temperaturom = tk.Button(
+            self.root,
+            text=TEMPERATURA_PLACEHOLDER,
         )
         self.button_upravljanje_temperaturom.place(
-            relx=0.5,
-            rely=(FIRST_REL_Y_PLACEMENT + 0.075 * 1),
+            relx=0.35,
+            rely=0.25,
+            height=BUTTON_HEIGHT,
             width=BUTTON_WIDTH,
-            anchor="center",
         )
 
         # Upravljanje kamerom - button
-        self.button_upravljanje_kamerom = ttk.Button(self.root, text=KAMERA_PLACEHOLDER)
+        self.button_upravljanje_kamerom = tk.Button(
+            self.root,
+            text=KAMERA_PLACEHOLDER,
+        )
         self.button_upravljanje_kamerom.place(
-            relx=0.5,
-            rely=(FIRST_REL_Y_PLACEMENT + 0.075 * 2),
+            relx=0.45,
+            rely=0.25,
+            height=BUTTON_HEIGHT,
             width=BUTTON_WIDTH,
-            anchor="center",
         )
 
-        # Upravljanje dodatnim metrikama - button
-        self.button_upravljanje_dodatnim_metrikama = ttk.Button(
-            self.root, text=DODATNE_METRIKE_PLACEHOLDER
+        # Upravljanje vlaznost - button
+        self.button_upravljanje_vlaznost = tk.Button(
+            self.root,
+            text=VLAZNOST_PLACEHOLDER,
         )
-        self.button_upravljanje_dodatnim_metrikama.place(
-            relx=0.5,
-            rely=(FIRST_REL_Y_PLACEMENT + 0.075 * 3),
+        self.button_upravljanje_vlaznost.place(
+            relx=0.55,
+            rely=0.25,
+            height=BUTTON_HEIGHT,
             width=BUTTON_WIDTH,
-            anchor="center",
         )
 
-        # Upravljanje lokacijom - button
-        self.button_upravljanje_lokacijom = ttk.Button(
-            self.root, text=LOKACIJA_PLACEHOLDER
-        )
-        self.button_upravljanje_lokacijom.place(
-            relx=0.5,
-            rely=(FIRST_REL_Y_PLACEMENT + 0.075 * 4),
+        # Upravljanje tlak - button
+        self.button_upravljanje_tlak = tk.Button(self.root, text=TLAK_PLACEHOLDER)
+        self.button_upravljanje_tlak.place(
+            relx=0.65,
+            rely=0.25,
+            height=BUTTON_HEIGHT,
             width=BUTTON_WIDTH,
-            anchor="center",
         )
+
+        # TODO: Dodati click eventove za svaki button - svaki button vodi na novi screen, osim vlaznosti i tlaka, oni samo prikazuju vrijednosti
+
+        # -----------------------------
+        # UPDATE-AJ PODATKE
+        # -----------------------------
+        self.root.after(1000, self.update_time)
+
+    def update_time(self):
+        trenutno_vrijeme = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.label_datum.config(text=f"{trenutno_vrijeme}")
+        self.root.after(1000, self.update_time)
